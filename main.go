215fae13d2fe50aa55ed43bdff37cb90517ab36b
Isabella334 <isabellarecinos@gmail.com>
2025-04-14 22:45:41 +0200

    fix: main package

--- main.go
+++ main.go
@@ -1,19 +1,10 @@
 package main // Ejecutable independiente
 
 import (
-	"net/http"
 	"github.com/gin-gonic/gin"
-	"errors"
 )
 
-type serie struct{
-	ID string
-	Title string
-	Episodes int
-	Rating float32
-
-}
-
 func main() {
-	
+	router := gin.Default()
+	router.Run(":8080")
 }
