cc0f5753b158d691431433a081b45924761eda1e
Isabella334 <isabellarecinos@gmail.com>
2025-05-16 14:37:30 -0600

    add: Basic models

---
+++ src/models.go
@@ -0,0 +1,20 @@
+package main
+
+import "time"
+
+type Series struct {
+  ID int `json:"id" gorm:"primaryKey"`
+  Title string `json:"title"`
+  Status string `json:"status"`
+  LastEpisodeWatched int `json:"lastEpisodeWatched" gorm:"column:last_episode_watched"`
+  TotalEpisodes int `json:"totalEpisodes" gorm:"column:total_episodes"`
+  Ranking int `json:"ranking"`
+  CreatedAt time.Time `json:"created_at"`
+  UpdatedAt time.Time `json:"updated_at"`
+}
+
+type ApiResponse struct {
+  Success bool `json:"success,omitempty"`
+  Message string `json:"message,omitempty"`
+  Data any `json:"data,omitempty"` 
+}
